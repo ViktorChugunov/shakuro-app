2d6295bb64c48c8daf7148c9304f2c02feea66f5
ViktorChugunov <viktor.chugunov@gmail.com>
2018-06-26 14:38:47 -0700

    new changes

--- src/index.js
+++ src/index.js
@@ -1,2 +1,53 @@
-require('./styles/style.scss');
-//require('./templates/main.haml');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const products_db = require('./products_db.json');
+
+
+window.showMoreProducts = function() {
+  var x=0;
+  var addedProductsLine = 
+    <div className="added-products-line">
+      <div className="products-preview-list-item">
+        <div className="product-group">{products_db[x]['product_name']}</div>
+        <div className="product-photo"><img src="" /></div>
+        <div className="product-name">{products_db[x]['product_name']}</div>
+        <div className="product-price">{products_db[x]['product_price']}</div>
+      </div>
+      <div className="products-preview-list-item">
+        <div className="product-group">{products_db[x+1]['product_name']}</div>
+        <div className="product-photo"><img src="" /></div>
+        <div className="product-name">{products_db[x+1]['product_name']}</div>
+        <div className="product-price">{products_db[x+1]['product_price']}</div>
+      </div>
+      <div className="products-preview-list-item">
+        <div className="product-group">{products_db[x+2]['product_name']}</div>
+        <div className="product-photo"><img src="" /></div>
+        <div className="product-name">{products_db[x+2]['product_name']}</div>
+        <div className="product-price">{products_db[x+2]['product_price']}</div>
+      </div>
+      <div className="products-preview-list-item">
+        <div className="product-group">{products_db[x+3]['product_name']}</div>
+        <div className="product-photo"><img src="" /></div>
+        <div className="product-name">{products_db[x+3]['product_name']}</div>
+        <div className="product-price">{products_db[x+3]['product_price']}</div>
+      </div>
+    </div>
+  ;
+  /*
+  for (let x = 0; x < 1; x++){
+    addedProductsLine += 
+      <div className="products-preview-list-item">
+        <div className="product-group">{products_db[x]['product_name']}</div>
+        <div className="product-photo"><img src="" /></div>
+        <div className="product-name">{products_db[x]['product_name']}</div>
+        <div className="product-price">{products_db[x]['product_price']}</div>
+      </div>;
+  } */
+  
+  ReactDOM.render(
+    addedProductsLine,
+    document.getElementById('added-products')
+  );
+}
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,81 @@
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var path = require("path");
 
 module.exports = {
-    entry: ['./src/index.js', 
-            './src/index.pug',
-    ],
-    output: {
-        filename: 'bundle.js'     
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    "style-loader",
-                    MiniCssExtractPlugin.loader,
-                    "css-loader"
-                ]
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    "style-loader",
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader"
-                ]
-            },
-            { 
-                test: /\.html\.haml$/,
-                loader: "haml-loader"
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'url-loader'
-                ]
-            },
-            {
-                test: /\.pug/,
-                loaders: [
-                    'html-loader',
-                    'pug-html-loader'
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: ['file-loader']
-            }
-
+  entry: ['./src/index.js', 
+          './src/index.pug',
+  ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js'     
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: [
+            "style-loader",
+            MiniCssExtractPlugin.loader,
+            "css-loader"
         ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "style.css"
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/index.pug',
-            filename: 'index.html'
-        })
-      ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      { 
+        test: /\.html\.haml$/,
+        loader: "haml-loader"
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+            'url-loader'
+        ]
+      },
+      {
+        test: /\.pug/,
+        loaders: [
+            'html-loader',
+            'pug-html-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    //compress: true,
+    stats: "errors-only",
+    open: true
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css"
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.pug',
+      filename: 'index.html'
+    })
+  ]
 };
